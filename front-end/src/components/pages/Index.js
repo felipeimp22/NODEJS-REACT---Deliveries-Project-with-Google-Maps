23a64592191fe0abf1a8dc9f353d5c8f459bdb5f
felipeimp22 <felipeimperio.imperioo@gmail.com>
2020-05-20 02:57:04 -0300

    page index and call google maps

--- front-end/src/components/pages/Index.js
+++ front-end/src/components/pages/Index.js
@@ -1,9 +1,73 @@
 import React from 'react';
 
+
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng
+} from "use-places-autocomplete"
+
+import {
+  Menu,
+  MenuList,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  MenuPopover,
+  MenuLink,
+} from "@reach/menu-button";
+// import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+
+
+
+// import "@reach/menu-button/styles.css";
+
+
 // import { Container } from './styles';
 
-function pages() {
-  return <div />;
+//   function Map() {
+//     return (
+//       <GoogleMap
+//         defaultZoom={10}
+//         defaultCenter={{ lat: -23.550520, lng: -46.633308 }}
+//       />
+//     )
+//   }
+// const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+
+// return <>
+//     <h1>teste</h1>
+//     {/* <div><WrappedMap googleMapURL={``} /></div> */}
+//   </>;
+
+
+
+const libraries = ["places"]
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+}
+const center = {
+  lat: -23.550520,
+  lng: -46.633308
 }
 
-export default pages;
+function Index() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "",
+    libraries,
+  })
+  /**
+   * Verificantions
+   */
+  if (loadError) return "Error Loading Maps";
+  if (!isLoaded) return "Loading Maps";
+
+
+  return <>
+    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={8} center={center} />
+  </>;
+}
+
+export default Index;
